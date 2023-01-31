a295f8bd610244914e83d2cc13e7b01664a58b0c
burnhambradley20 <116041544+burnhambradley20@users.noreply.github.com>
2023-01-31 15:43:59 -0600

    just need to finish routes

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+    try {
+      const libraryCardData = await LibraryCard.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      if (!libraryCardData) {
+        res.status(404).json({ message: 'No library card found with that id!' });
+        return;
+      }
+
+      res.status(200).json(libraryCardData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  // delete on tag by its `id` value
+
+module.exports = router;
